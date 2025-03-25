ce2f0858d47d6718277492b698412a97146a9e33
Adam <NayJi7@github.com>
2025-03-25 10:56:41 +0100

    Revert "setups update"
    This reverts commit 4a93623bdf3d06c3520dc37d54bf520eaca0018d.

--- setup.bat
+++ setup.bat
@@ -1,23 +1,20 @@
 @echo off
-chcp 65001 > nul
-
 echo.
 echo === Installation de CyCalendar pour Windows ===
-echo Demarrage de l'installation...
+echo Démarrage de l'installation...
 
 echo.
-echo Installation des dependances Python...
-echo Cette operation peut prendre plusieurs minutes selon votre connexion internet
+echo Installation des dépendances Python...
+echo Cette opération peut prendre plusieurs minutes selon votre connexion internet
 pip install --upgrade pip
 pip install colorama
 pip install -r requirements.txt
 
 echo.
-echo Creation des repertoires necessaires...
+echo Création des répertoires nécessaires...
 if not exist google mkdir google
 if not exist generated mkdir generated
 
 echo.
-echo Installation terminee !
-set PYTHONIOENCODING=utf-8
-echo Lancement du script principal...
+echo Installation terminée !
+pause
